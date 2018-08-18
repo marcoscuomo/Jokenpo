69a65fbab89476162a8f7d9ef93fafed552e7165
bytecore@gmail.com <mo10128>
2018-08-18 13:28:14 -0300

    Iniciando atributos e variaveis

--- app/src/main/java/br/com/mojumob/jokenpo/MainActivity.java
+++ app/src/main/java/br/com/mojumob/jokenpo/MainActivity.java
@@ -2,12 +2,25 @@ package br.com.mojumob.jokenpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Atributos
+    private ImageView imgPedra, imgPapel, imgTesoura, imgComputador;
+    private TextView txtResultador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicializações
+        imgPedra      = findViewById(R.id.imgPedra);
+        imgPapel      = findViewById(R.id.imgPapel);
+        imgTesoura    = findViewById(R.id.imgTesoura);
+        imgComputador = findViewById(R.id.imgComputador);
+        txtResultador = findViewById(R.id.txtResultado);
     }
 }
